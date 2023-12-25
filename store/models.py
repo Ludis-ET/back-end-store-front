e6e4f549924560862cfe1cc65799e3a240f4f75d
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-12-25 10:59:01 +0300

    creating likes

--- store/models.py
+++ store/models.py
@@ -16,7 +16,7 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now = True)
     collection = models.ForeignKey(Collection,on_delete=models.PROTECT)
-    promotions = models.ManyToManyField(Promotion,'''related_name="products"''')
+    promotions = models.ManyToManyField(Promotion)#related_name="products
 
 
 class Customer(models.Model):
@@ -46,7 +46,7 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order,on_delete=models.PROTECT)
-    product = models.ForeignKey(Order,on_delete=models.PROTECT)
+    product = models.ForeignKey(Product,on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places = 2)
 
