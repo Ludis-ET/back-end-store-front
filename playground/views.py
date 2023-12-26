0f592d52269160a38e5df58f9d32d2fdfc4343f0
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-12-26 18:49:20 +0300

    today

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from store.models import Product
+from django.db.models import Q,F
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def index(request):
-    query_set = Product.objects.all()
-    list(query_set)
-    
-    return render(request,'index.html')
+    # try:
+    #     query_set = Product.objects.get(pk=0)
+    # except ObjectDoesNotExist:
+    #     pass
+    # list(query_set)
+    query_set = Product.objects.order_by('-title')
+
+    return render(request,'index.html',{'products':list(query_set)})
