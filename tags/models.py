e2a6dc58ce96a51de70330edfff27c5cc31781c0
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-12-28 09:52:30 +0300

    adding tagged manager

--- tags/models.py
+++ tags/models.py
@@ -2,6 +2,18 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
+class TaggedItemManager(models.Manager):
+    def ger_tags_for(self,obj_type,obj_id):
+        content_type = ContentType.objects.get_for_model(obj_type)
+
+        queryset = TaggedItem.objects \
+            .select_related('tag') \
+            .filter(
+                content_type=content_type,
+                object_id = obj_id
+            )
+
+
 class Tag(models.Model):
     lavel = models.CharField(max_length=255)
 
