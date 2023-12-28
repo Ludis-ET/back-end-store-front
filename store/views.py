ef46f6445ab3d6d5d3561deee0d863b949553343
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-12-28 16:56:24 +0300

    adding 404 page to the api

--- store/views.py
+++ store/views.py
@@ -13,6 +13,9 @@ def product_list(request):
 
 @api_view()
 def product_detail(request,id):
-    product = Product.objects.get(pk=id)
-    serializer = ProductSterializer(product)
-    return Response(serializer.data)
+    try:
+        product = Product.objects.get(pk=id)
+        serializer = ProductSterializer(product)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+         return Response(status=404)
