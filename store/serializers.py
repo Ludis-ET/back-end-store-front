6474402a101a4121792d3839c87f871739ea866d
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-12-28 17:36:54 +0300

    usign meta class lesson 14

--- store/serializers.py
+++ store/serializers.py
@@ -7,15 +7,18 @@ class CollectionSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
 
 
-class ProductSterializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6,decimal_places=2)
-    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.HyperlinkedRelatedField(
-        queryset = Collection.objects.all(),
-        view_name='collection-detail'
-    )
+class ProductSterializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','title','unit_price','collection']
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # unit_price = serializers.DecimalField(max_digits=6,decimal_places=2)
+    # price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset = Collection.objects.all(),
+    #     view_name='collection-detail'
+    # )
 
     def calculate_tax(self,product: Product):
         return product.unit_price * Decimal(1.1)
