0237d4283f069492243690cbca4412b8cbbbf404
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-12-28 10:24:05 +0300

    adimin

--- storefront/urls.py
+++ storefront/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
 
+
+admin.site.site_header = 'Ludis Admin'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('playground.urls')), 
