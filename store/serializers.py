cc5e6970e1557c9b41c7ca3bebe8dd115c83be4b
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-12-29 16:07:30 +0300

    altering serializer

--- store/serializers.py
+++ store/serializers.py
@@ -2,19 +2,20 @@ from rest_framework import serializers
 from .models import *
 from decimal import Decimal
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id','title']
 
 
 class ProductSterializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id','title','unit_price','collection']
+        fields = ['id','title','unit_price','price_with_tax','collection'] # '__all__'  bad practice
     # id = serializers.IntegerField()
     # title = serializers.CharField(max_length=255)
     # unit_price = serializers.DecimalField(max_digits=6,decimal_places=2)
-    # price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     # collection = serializers.HyperlinkedRelatedField(
     #     queryset = Collection.objects.all(),
     #     view_name='collection-detail'
