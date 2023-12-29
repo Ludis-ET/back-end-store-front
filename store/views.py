9c90a1c7581ab97c7770601fa637197227c252e4
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-12-29 16:53:06 +0300

    deleting objects

--- store/views.py
+++ store/views.py
@@ -22,7 +22,7 @@ def product_list(request):
         return Response(serializer.data,status=status.HTTP_201_CREATED)
     
 
-@api_view(['GET','PUT'])
+@api_view(['GET','PUT','DELETE'])
 def product_detail(request,id): 
     product = get_object_or_404(Product,pk=id)
     if request.method == 'GET':
@@ -33,6 +33,12 @@ def product_detail(request,id):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        if product.orderitem_set.count() > 0:
+            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
     
 
 @api_view()
