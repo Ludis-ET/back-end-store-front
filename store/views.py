55dfe31277b6a6ec914a5210e0e40500b86636af
Ludis-ET <leulsegedmelaku1020@gmail.com>
2024-01-06 12:23:03 +0300

    changing product list to generic view

--- store/views.py
+++ store/views.py
@@ -5,21 +5,16 @@ from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import ListCreateAPIView
 from .models import Collection, Product
 from .serializers import CollectionSerializer, ProductSerializer
 
 
-class ProductList(APIView):
-    def get(self,request):
-        queryset = Product.objects.select_related('collection').all()
-        serializer = ProductSerializer(
-            queryset, many=True, context={'request': request})
-        return Response(serializer.data)
-    def post(self,request):
-        serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class ProductList(ListCreateAPIView):
+    queryset = Product.objects.select_related('collection').all()
+    serializer_class = ProductSerializer
+    def get_serializer_context(self):
+        return {'request':self.request}
         
 
 class ProductDetail(APIView):
