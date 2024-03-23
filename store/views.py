525b4daa69ac3be5d48d81c713ec5dc0af18c38f
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-23 09:30:49 +0300

    "Add REST API view for product list in store app and update settings; update pyc and py files in pycache"
    This commit includes changes to the `views.py` file in the `store` app, where a new REST API view is added to return a list of products using the Django Rest Framework. The `settings.py` file in the project root directory is also updated to include the REST framework configuration. Additionally, the `urls.py` file in the project root directory is updated to include the `store` app URLs. The `pycache` directory contains updated pyc and py files due to the changes made in the corresponding source files.

--- store/views.py
+++ store/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+@api_view()
+def product_list(request):
+    queryset = Product.objects.all()
+    serializer = ProductSerializer(queryset,many = True)
+    return Response(serializer.data)
