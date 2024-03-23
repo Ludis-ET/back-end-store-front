3e95bcb4f48063b475e709457fe32c2ffec7d94b
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-23 10:22:32 +0300

    Update models, serializers, views, and URLs for product and collection management
    * Modify Product and OrderItem models to include related\_name for collection and product foreign keys
    * Add Collection model to ProductSerializer and calculate tax as a SerializerMethodField
    * Update views to include collection-related views and add corresponding URLs
    * Add CollectionSerializer and related views and URLs
    * Add exception handling for DELETE requests in product\_detail and collection\_detail views
    * Update URLConf to include new product and collection views.

--- store/serializers.py
+++ store/serializers.py
@@ -1,7 +1,25 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Product
+from decimal import Decimal
+from store.models import Product, Collection
+from rest_framework import serializers
 
-class ProductSerializer(ModelSerializer):
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title', 'products_count']
+
+    products_count = serializers.IntegerField()
+
+
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('id', 'title', 'unit_price')
+        fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
+
+    price_with_tax = serializers.SerializerMethodField(
+        method_name='calculate_tax')
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
+
+   
